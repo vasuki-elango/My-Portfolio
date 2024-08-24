2ce944d53126924e8caa692b48840b40bb89f1a3
vasuki-elango <vasukie96@gmail.com>
2024-08-24 12:27:50 +0530

    upload project

--- src/Components/Project.jsx
+++ src/Components/Project.jsx
@@ -14,13 +14,41 @@ export const Project = () => {
         <h2 className="sectionTitle">My Portfolio</h2>
         <span className="sectionsubtitle">Creating dynamic and engaging fronnd-end experiences with modern web technologies,developing robust Java applications,and desiging intuitive UI/UX interfaces.My Projects blend functionality and aesthetics to deliver seamless,user-cenrric solutions that address real-world challenges and drive impactful results.</span>
         <div className="workings">
-            <a href="https://vasuki-elango.github.io/LibraryManagement/" target='__blank'><img src={Library} alt="" /></a>
-            <a href="https://vasuki-organic-site.netlify.app/" target='__blank'><img src={organic} alt="" /></a>
-            <a href="https://vasuki-elango.github.io/QuizApplication/" target='__blank'><img src={Quizapp} alt="" /></a>
-            <a href="https://www.figma.com/design/tjhq4D06q6o6kJOUiA3Tp7/GYM-WEBSITE?node-id=0-1&t=fXWU6cH5GSMyZMFg-1" target='__blank'><img src={gym} alt="" /></a>
-            <a href="https://vasuki-floweshop.netlify.app/"><img src={flower} alt="" /></a>
-            <a href="https://vasuki-floweshop.netlify.app/"><img src={recipe} alt="" /></a>
-            <a href="https://www.figma.com/design/KS88bXjpcoQKAeDWm2Bvwm/anime?node-id=0-1&t=yvnmqPYMzAbbN574-1" target='__blank'><img src={anime} alt="" /></a>
+          <div className="projects-field">
+            <h3>Library Management</h3>
+            <img src={Library} alt="" />
+            <a href="https://vasuki-elango.github.io/LibraryManagement/" target='__blank'>Live Demo</a>
+          </div>
+          <div className="projects-field">
+            <h3>Organic Site</h3>
+            <img src={organic} alt="" />
+            <a href="https://vasuki-organic-site.netlify.app/" target='__blank'>Live Demo</a>
+          </div>
+          <div className="projects-field">
+            <h3>Flower Shop</h3>
+            <img src={flower} alt="" />
+            <a href="https://vasuki-floweshop.netlify.app/">Live Demo</a>
+          </div>
+          <div className="projects-field">
+            <h3>Quiz App</h3>
+            <img src={Quizapp} alt="" />
+            <a href="https://vasuki-elango.github.io/LibraryManagement/" target='__blank'>Live Demo</a>
+          </div>
+          <div className="projects-field">
+            <h3>Recipe Finder</h3>
+            <img src={recipe} alt="" />
+            <a href="https://vasuki-elango.github.io/Recipe-Finder/">Live Demo</a>
+          </div>
+          <div className="projects-field">
+            <h3>Gym Website</h3>
+            <img src={gym} alt="" />
+            <a href="https://www.figma.com/design/tjhq4D06q6o6kJOUiA3Tp7/GYM-WEBSITE?node-id=0-1&t=fXWU6cH5GSMyZMFg-1" target='__blank'>Live Demo</a>
+          </div>
+          <div className="projects-field">
+            <h3>Anime Website</h3>
+            <img src={anime} alt="" />
+            <a href="https://www.figma.com/design/KS88bXjpcoQKAeDWm2Bvwm/anime?node-id=0-1&t=yvnmqPYMzAbbN574-1" target='__blank'>Live Demo</a>
+          </div>
         </div>
     </section>
   )
